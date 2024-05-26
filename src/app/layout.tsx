db8cd89364d3042be4470655887894c0453ced65
Titian31 <titian.p123123@gmail.com>
2024-05-26 19:32:39 +0700

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
+import { Suspense } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +21,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
+        <Suspense>
         {children}
+        </Suspense>
       </body>
     </html>
   );
